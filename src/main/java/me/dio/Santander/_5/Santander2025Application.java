21f9bb7b1e275d1cd5e1ffe6b81061f5119bfd49
Italo Coutinho <gstack@MacBook-Pro-de-Italo.local>
2025-03-12 11:42:46 -0300

    first commit

---
+++ src/main/java/me/dio/Santander/_5/Santander2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander._5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander/_5/Santander2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
